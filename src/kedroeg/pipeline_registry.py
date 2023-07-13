f93b9f3a5e035fc4e897d39eedd18653336b76b1
Toran Sahu <toran.sahu@yahoo.com>
2023-07-13 14:45:49 +0530

    register the reqres pipeline

--- src/kedroeg/pipeline_registry.py
+++ src/kedroeg/pipeline_registry.py
@@ -3,6 +3,8 @@ from typing import Dict
 
 from kedro.pipeline import Pipeline, pipeline
 
+from kedroeg.pipelines.reqres import reqres_list_users
+
 
 def register_pipelines() -> Dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -10,4 +12,7 @@ def register_pipelines() -> Dict[str, Pipeline]:
     Returns:
         A mapping from a pipeline name to a ``Pipeline`` object.
     """
-    return {"__default__": pipeline([])}
+    return {
+        "reqres_list_users": reqres_list_users(),
+        "__default__": pipeline([]),
+    }
